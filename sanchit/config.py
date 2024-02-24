e3d21a56281e320f66ba9585254e553983f5a006
Unstoppable Sanchit <137860157+Sanchit0102@users.noreply.github.com>
2024-02-24 23:08:09 +0530

    Update config.py

--- sanchit/config.py
+++ sanchit/config.py
@@ -12,7 +12,7 @@ class Telegram:
     SECRET_CODE_LENGTH = int(env.get("SECRET_CODE_LENGTH", 12))
 
 class Server:
-    BASE_URL = env.get("BASE_URL", "")
+    BASE_URL = env.get("BASE_URL", "https://leech-with-silent-0233e98bcbd6.herokuapp.com")
     BIND_ADDRESS = env.get("BIND_ADDRESS", "0.0.0.0")
     PORT = int(env.get("PORT", 8080))
 
