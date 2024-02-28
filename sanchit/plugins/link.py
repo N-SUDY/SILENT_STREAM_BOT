8af119da035118f7f130a9b1ece2087f0427e662
Unstoppable Sanchit <137860157+Sanchit0102@users.noreply.github.com>
2024-02-28 12:13:05 +0530

    Update link.py

--- sanchit/plugins/link.py
+++ sanchit/plugins/link.py
@@ -117,6 +117,13 @@ async def gen_link_handler(event):
             media = reply_message.file
             file_name = media.name
             file_path = await reply_message.download_media(file_name)
+<<<<<<< new_changes
+
+            log_msg = await Sanchit.send_file(Telegram.CHANNEL_ID, file_path, caption=msg_text.format(file_name, humanbytes(media.size), f'{base_url}/watch/{reply_message.id}/{quote_plus(get_name(reply_message))}?hash={await get_hash(reply_message)}', f'{base_url}/{reply_message.id}/{quote_plus(get_name(reply_message))}?hash={await get_hash(reply_message)}'))
+            os.remove(file_path)
+
+            await event.reply(text=msg_text.format(file_name, humanbytes(media.size), f'{Server.BASE_URL}/file/{message_id}?code={secret_code}', f'{Server.BASE_URL}/dl/{message_id}?code={secret_code}', reply_markup=types.ReplyKeyboardMarkup([[types.KeyboardButton("sᴛʀᴇᴀᴍ🔺"), types.KeyboardButton('ᴅᴏᴡɴʟᴏᴀᴅ🔻')]]))
+=======
             stream_link = f'{Server.BASE_URL}/stream/{message_id}?code={secret_code}'
             dl_link = f'{Server.BASE_URL}/dl/{message_id}?code={secret_code}'
             
@@ -125,6 +132,7 @@ async def gen_link_handler(event):
 
             
     await event.reply(text=msg_text.format(get_name(media)), humanbytes(media.size), stream_link, dl_link, reply_markup=types.ReplyKeyboardMarkup([[types.KeyboardButton("sᴛʀᴇᴀᴍ🔺", url=stream_link), types.KeyboardButton('ᴅᴏᴡɴʟᴏᴀᴅ🔻', url=dl_link)]]))
+>>>>>>> main
         else:
             await event.reply("Reply to a valid media file with /link to generate a download link.")
     except Exception as e:
