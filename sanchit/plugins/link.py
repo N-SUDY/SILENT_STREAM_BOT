7b994f5ef77b0fcf9374c007e26097478c40d5c0
Unstoppable Sanchit <137860157+Sanchit0102@users.noreply.github.com>
2024-02-28 12:57:50 +0530

    Update link.py

--- sanchit/plugins/link.py
+++ sanchit/plugins/link.py
@@ -73,7 +73,7 @@ async def gen_link_handler(event):
             message = await send_message(event.message)
             message_id = message.id
 
-            log_msg = await Sanchit.send_file(Telegram.CHANNEL_ID, file_path, caption=msg_text.format(file_name, humanbytes(media.size), f'{base_url}/watch/{reply_message.id}/{quote_plus(get_name(reply_message))}?hash={await get_hash(reply_message)}', f'{base_url}/{reply_message.id}/{quote_plus(get_name(reply_message))}?hash={await get_hash(reply_message)}'))
+            log_msg = await Sanchit.send_file(Telegram.CHANNEL_ID, file_path, caption=msg_text.format(file_name, humanbytes(media.size), f'{Server.BASE_URL}/stream/{message_id}?code={secret_code}', f'{Server.BASE_URL}/dl/{message_id}?code={secret_code}'))
             os.remove(file_path)
 
             await event.reply(text=msg_text.format(file_name, humanbytes(media.size), f'{Server.BASE_URL}/file/{message_id}?code={secret_code}', f'{Server.BASE_URL}/dl/{message_id}?code={secret_code}', reply_markup=types.ReplyKeyboardMarkup([[types.KeyboardButton("sᴛʀᴇᴀᴍ🔺"), types.KeyboardButton('ᴅᴏᴡɴʟᴏᴀᴅ🔻')]]))
